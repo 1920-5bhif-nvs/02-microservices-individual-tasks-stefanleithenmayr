b422274e0bddcc6669918def974a884c3258487e
stefanleithenmayr <s.leithenmayr@gmx.at>
2019-10-08 08:20:26 +0200

    project created

---
+++ cardealer-service/src/test/java/at/htl/services/car/controller/NativeEmployeeControllerIT.java
@@ -0,0 +1,9 @@
+package at.htl.services.car.controller;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeEmployeeControllerIT extends EmployeeControllerTest {
+
+    // Execute the same tests but in native mode.
+}
